64a5fb9afe63e4c7e9d7ae41ed7475f42fc7bcb0
Stuart Claghorn <sclaghorn@gmail.com>
2014-07-29 23:36:32 -0400

    Initial creation of sample app for Csc193b Stanford

--- Attributor/AttributorViewController.m
+++ Attributor/AttributorViewController.m
@@ -9,10 +9,18 @@
 #import "AttributorViewController.h"
 
 @interface AttributorViewController ()
+@property (weak, nonatomic) IBOutlet UITextView *body;
+@property (weak, nonatomic) IBOutlet UILabel *headLine;
 
 @end
 
 @implementation AttributorViewController
+- (IBAction)setSelectedBodyTextAttribute:(UIButton *)sender
+{
+    [self.body.textStorage addAttribute:NSForegroundColorAttributeName
+                                  value:sender.backgroundColor
+                                  range:self.body.selectedRange];
+}
 
 - (void)viewDidLoad
 {
